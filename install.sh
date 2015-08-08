f9ba2baf51baf738a3723354e98334f04dcc5b26
eabderh <eabderh@clemson.edu>
2015-08-08 13:27:51 +0000

    squash

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-instpath="$HOME/dev/config_files/"
-homepath=$PWD/
-
-#mkdir $instpath
-
-#git clone http://github.com/eabderh/config_files.git
-
 regex_esc () {
 	echo "$1" | sed -e 's/[]\/$*.^|[]/\\&/g'
 }
+
+
+
+instpath="$HOME/dev/config_files/"
+homepath=$PWD/
+
+mkdir -p "$instpath"
+cd "$instpath"
+
+git clone https://github.com/eabderh/config_files.git
 
 homepath_regex=$(regex_esc "$homepath")
 instpath_regex=$(regex_esc "$instpath")
 sedcom="s/^\(.*\)$/$instpath_regex\1 $homepath_regex\1/"
 
-cd "$instpath"
 git ls-tree -r -d  HEAD --name-only --full-tree \
  	| sed -e "s/^/$homepath_regex/" \
-	| xargs -n 1 mkdir
+	| xargs -n 1 mkdir -p
 git ls-tree -r HEAD --name-only --full-tree \
 	| sed -e "$sedcom" \
 	| xargs -t -n 2 sh -c 'ln -s "$1" "$2"' argv0
