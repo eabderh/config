fba561956dbf16786fd90b26739a80f53ff4bf22
eabderh <eabderh@clemson.edu>
2015-08-08 16:21:03 +0000

    added vim plugins installation
    main vundle repo is now a submodule in the main git repo.
    vundle running as installer of plugins is buggy.

--- install.sh
+++ install.sh
@@ -5,25 +5,36 @@ regex_esc () {
 }
 
 
+homepath=$PWD
+instpath="$homepath/dev/config_files"
 
-instpath="$HOME/dev/config_files/"
-homepath=$PWD/
+homepath_regex=$(regex_esc "$homepath/")
+instpath_regex=$(regex_esc "$instpath/")
+sedcom="s/^\(.*\)$/$instpath_regex\1 $homepath_regex\1/"
+
 
 mkdir -p "$instpath"
 cd "$instpath"
 
-git clone https://github.com/eabderh/config_files.git
+git clone -b dev --recursive \
+	https://github.com/eabderh/config_files.git "$instpath"
 
-homepath_regex=$(regex_esc "$homepath")
-instpath_regex=$(regex_esc "$instpath")
-sedcom="s/^\(.*\)$/$instpath_regex\1 $homepath_regex\1/"
+#ln -s "$instpath/.vim" "$HOME/.vim"
 
 git ls-tree -r -d  HEAD --name-only --full-tree \
  	| sed -e "s/^/$homepath_regex/" \
 	| xargs -n 1 mkdir -p
+#	| sed -e "/\/\.vim$/d" \
+
+rm -rf "$homepath/.vim/bundle/Vundle.vim"
+ln -s "$instpath/.vim/bundle/Vundle.vim" "$homepath/.vim/bundle/Vundle.vim"
+
 git ls-tree -r HEAD --name-only --full-tree \
 	| sed -e "$sedcom" \
 	| xargs -t -n 2 sh -c 'ln -s "$1" "$2"' argv0
+#	| sed -e "/\/\.vim\//d" \
 exit 0
+
+vim -c 'PluginInstall'
 
 
