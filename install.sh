caed77b5c746e3056146dd4d7b18e03e0f75d0df
eabderh <eabderh@clemson.edu>
2015-08-08 07:06:36 +0000

    delete: dont need awk to add text to file
    using this as a temporary saving place.

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+inst_path="$HOME/dev/config_files/"
+
+#mkdir $inst_path
+#cd ~/dev/config_files
+cd "$inst_path"
+
+#git clone http://github.com/eabderh/config_files.git
+
+find $inst_path -maxdepth 1 -type f -name ".*" -print \
+	| awk -vi="$inst_path" '$0=i$0' \
+	| xargs echo
+#	| awk '{ print $inst_path $0 }' \
 
 
+find $inst_path -maxdepth 1 -type f -name ".*" -print \
+	| awk -vi="$inst_path" '$0=i$0'
+
+find $inst_path -maxdepth 1 -type f -name ".*"
