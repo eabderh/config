7c6016bc1021c13b884253139feb006e4fa82554
eabderh <eabderh@clemson.edu>
2015-08-08 11:17:19 +0000

    finished up on creating symlinks
    directories are now made instead of symlinked.
    symlinks are created.
    directories and files linked are the ones tracked by git.

--- install.sh
+++ install.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-inst_path="$HOME/dev/config_files/"
+instpath="$HOME/dev/config_files/"
+homepath=$PWD/
 
-#mkdir $inst_path
-#cd ~/dev/config_files
-cd "$inst_path"
+#mkdir $instpath
 
 #git clone http://github.com/eabderh/config_files.git
 
-find $inst_path -maxdepth 1 -type f -name ".*" -print \
-	| awk -vi="$inst_path" '$0=i$0' \
-	| xargs echo
-#	| awk '{ print $inst_path $0 }' \
+regex_esc () {
+	echo "$1" | sed -e 's/[]\/$*.^|[]/\\&/g'
+}
+
+homepath_regex=$(regex_esc "$homepath")
+instpath_regex=$(regex_esc "$instpath")
+sedcom="s/^\(.*\)$/$instpath_regex\1 $homepath_regex\1/"
+
+cd "$instpath"
+git ls-tree -r -d  HEAD --name-only --full-tree \
+ 	| sed -e "s/^/$homepath_regex/" \
+	| xargs -n 1 mkdir
+git ls-tree -r HEAD --name-only --full-tree \
+	| sed -e "$sedcom" \
+	| xargs -t -n 2 sh -c 'ln -s "$1" "$2"' argv0
+exit 0
 
 
-find $inst_path -maxdepth 1 -type f -name ".*" -print \
-	| awk -vi="$inst_path" '$0=i$0'
-
-find $inst_path -maxdepth 1 -type f -name ".*"
